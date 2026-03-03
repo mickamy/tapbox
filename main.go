b2ea4dc00472841ac3a770d6067c9d6d3b720416
Tetsuro Mikami <tetsuro@mickamy.com>
2026-03-03 14:45:23 +0900

    feat: wire up main.go

--- main.go
+++ main.go
@@ -1,9 +1,149 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"golang.org/x/sync/errgroup"
+	"google.golang.org/grpc"
+
+	"github.com/mickamy/tapbox/internal/config"
+	grpcproxy "github.com/mickamy/tapbox/internal/proxy/grpc"
+	httpproxy "github.com/mickamy/tapbox/internal/proxy/http"
+	sqlproxy "github.com/mickamy/tapbox/internal/proxy/sql"
+	"github.com/mickamy/tapbox/internal/trace"
+	"github.com/mickamy/tapbox/internal/ui"
+)
 
 var version = "dev"
 
+const readHeaderTimeout = 10 * time.Second
+
 func main() {
-	fmt.Println("tapbox", version)
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "tapbox: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	cfg, err := config.Parse(os.Args[1:])
+	if err != nil {
+		return fmt.Errorf("parsing config: %w", err)
+	}
+
+	store := trace.NewMemStore(cfg.MaxTraces)
+	collector := trace.NewCollector(store)
+	correlator := sqlproxy.NewCorrelator(0)
+
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	g, ctx := errgroup.WithContext(ctx)
+
+	// HTTP proxy
+	httpProxy, err := httpproxy.NewProxy(cfg.HTTPTarget, collector, cfg.MaxBodySize)
+	if err != nil {
+		return fmt.Errorf("http proxy: %w", err)
+	}
+	httpProxy.OnSpan = func(traceID, spanID string) {
+		correlator.SetActive(traceID, spanID)
+	}
+
+	g.Go(func() error {
+		srv := &http.Server{
+			Addr:              cfg.HTTPListen,
+			Handler:           httpProxy,
+			ReadHeaderTimeout: readHeaderTimeout,
+		}
+		go func() {
+			<-ctx.Done()
+			if closeErr := srv.Close(); closeErr != nil {
+				log.Printf("http proxy close: %v", closeErr)
+			}
+		}()
+		log.Printf("HTTP proxy listening on %s -> %s", cfg.HTTPListen, cfg.HTTPTarget)
+		if srvErr := srv.ListenAndServe(); srvErr != nil && srvErr != http.ErrServerClosed {
+			return fmt.Errorf("http proxy: %w", srvErr)
+		}
+		return nil
+	})
+
+	// gRPC proxy
+	if cfg.EnableGRPC {
+		gp, gpErr := grpcproxy.NewProxy(cfg.GRPCTarget, collector)
+		if gpErr != nil {
+			return fmt.Errorf("grpc proxy: %w", gpErr)
+		}
+		grpcServer := grpc.NewServer(grpc.UnknownServiceHandler(gp.UnknownHandler()))
+
+		g.Go(func() error {
+			var lc net.ListenConfig
+			ln, lisErr := lc.Listen(ctx, "tcp", cfg.GRPCListen)
+			if lisErr != nil {
+				return fmt.Errorf("grpc listen: %w", lisErr)
+			}
+			go func() {
+				<-ctx.Done()
+				grpcServer.GracefulStop()
+			}()
+			log.Printf("gRPC proxy listening on %s -> %s", cfg.GRPCListen, cfg.GRPCTarget)
+			return grpcServer.Serve(ln)
+		})
+	}
+
+	// SQL proxy
+	if cfg.EnableSQL {
+		sp := sqlproxy.NewProxy(cfg.SQLTarget, collector, correlator)
+		if lisErr := sp.Listen(ctx, cfg.SQLListen); lisErr != nil {
+			return fmt.Errorf("sql proxy: %w", lisErr)
+		}
+		g.Go(func() error {
+			go func() {
+				<-ctx.Done()
+				if closeErr := sp.Close(); closeErr != nil {
+					log.Printf("sql proxy close: %v", closeErr)
+				}
+			}()
+			log.Printf("SQL proxy listening on %s -> %s", cfg.SQLListen, cfg.SQLTarget)
+			return sp.Serve()
+		})
+	}
+
+	// UI server
+	uiServer := ui.NewServer(store, cfg.ExplainDSN)
+	uiServer.Start(ctx)
+
+	g.Go(func() error {
+		srv := &http.Server{
+			Addr:              cfg.UIListen,
+			Handler:           uiServer.Handler(),
+			ReadHeaderTimeout: readHeaderTimeout,
+		}
+		go func() {
+			<-ctx.Done()
+			if closeErr := srv.Close(); closeErr != nil {
+				log.Printf("ui server close: %v", closeErr)
+			}
+		}()
+		log.Printf("UI available at http://localhost%s", cfg.UIListen)
+		if srvErr := srv.ListenAndServe(); srvErr != nil && srvErr != http.ErrServerClosed {
+			return fmt.Errorf("ui server: %w", srvErr)
+		}
+		return nil
+	})
+
+	log.Printf("tapbox %s started", version)
+
+	if waitErr := g.Wait(); waitErr != nil {
+		return fmt.Errorf("server error: %w", waitErr)
+	}
+	return nil
 }
